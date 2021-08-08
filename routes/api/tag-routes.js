3183dc9e798beb56e87789f6ec2f501d4571acd8
Kelebet Engida <engida398@gmail.com>
2021-08-08 14:15:55 -0700

    All routes are complete and sequlize has been synced.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,25 +6,62 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-
+  Tag.findAll({
+    include:{
+      model:Product
+    }
+    .then ((tags)=> res.json(tags))
+    .cath((err)=> res.status(500).json(err)) 
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  
+  Tag.findOne({
+    where:{
+      id: req.params.id
+    },
+    include:{
+      model:Product
+    }
+    .then ((tags)=> res.json(tags))
+    .cath((err)=> res.status(500).json(err))
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then ((tags)=> res.json(tags))
+  .cath((err)=> res.status(500).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+  },
+  {
+    where:{
+      id: req.params.id
+    }
+  })
+  .then ((tags)=> res.json(tags))
+  .cath((err)=> res.status(500).json(err))
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.delete({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then ((tags)=> res.json(tags))
+  .cath((err)=> res.status(500).json(err))
 });
 
 module.exports = router;
